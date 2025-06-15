7c5c3c93e4c4191dbd3e6b0cf67ee739769e284d
ymail8372 <ymail8372@naver.com>
2025-06-15 21:21:20 +0900

    Add wifi_control_device code

--- light_sensor_skeleton/light_sensor_skeleton.ino
+++ light_sensor_skeleton/light_sensor_skeleton.ino
@@ -8,9 +8,7 @@ void setup() {
 
   // ESP32의 I2C 핀 할당 (SDA: 21, SCL: 22)
   Wire.begin(21, 22);
-  if(lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
-    Serial.println("true!");
-  }
+  lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE);
   // BH1750 센서 초기화 (고해상도 연속 측정 모드)
   // if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
   //   Serial.println("BH1750 Sensor initialized.");
